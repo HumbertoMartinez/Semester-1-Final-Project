6663df14487a7d847584a491f9e64c1f7a76a7e1
HumbertoMartinez <9hmartinez@stpatrick.org>
2016-12-13 15:23:38 -0600

    day2

--- First-Semester-Final-master/cart.js
+++ First-Semester-Final-master/cart.js
@@ -1,9 +1,19 @@
 class Cart{
   //What is the first part of every class? Type it below.
-
-
+constructor(itemList,itemQuantity){
+  this.itemList = itemList;
+  this.itemQuantity = itemQuantity;
+}
 
   //Type the instance functions below this comment.
+addItem(i,q){
+  this.itemList.push(i);
+  this.itemQuantity.push(q);
+}
+totalCart(){
+  let total = 0;
+  for(a=0;a<itemList.length;a++)
 
+  item.push(total);
 
 }
