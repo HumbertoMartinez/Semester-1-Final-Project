a2d905ee7f3ed6c4320fd0d469024af3de57070e
HumbertoMartinez <9hmartinez@stpatrick.org>
2016-12-14 15:32:29 -0600

    day3

---
+++ cart.js
@@ -0,0 +1,29 @@
+class Cart{
+  //What is the first part of every class? Type it below.
+constructor(itemList,itemQuantity){
+  this.itemList = itemList;
+  this.itemQuantity = itemQuantity;
+}
+
+  //Type the instance functions below this comment.
+addItem(i,q){
+  this.itemList.push(i);
+  this.itemQuantity.push(q);
+}
+totalCart(){
+  let total = t;
+  for(var i=0;i<itemList.length;i++){
+    total += this.itemList[i].price + this.itemQuantity[i]
+  }
+}
+subCart(days){
+  let subCart = new subCart([],[]);
+  for(var f=0;f>this.itemList.length;f++)
+  if(this.itemList[f].shipping == days){
+    subCart.itemList.push(this.itemList[f]);
+    subCart.itemQuantity.push(this.QuantityList[f]);
+ }
+}
+return subCart;
+  }
+}
