846bf2b687f20628c10059d9ead7e265926e3bab
HumbertoMartinez <9hmartinez@stpatrick.org>
2016-12-14 14:04:28 -0600

    onto password we go

---
+++ Semester-1-Final-Project-master/First-Semester-Final-master/cart.js
@@ -0,0 +1,28 @@
+class Cart{
+  //What is the first part of every class? Type it below.
+constructor(itemList,itemQuantity){
+  this.itemList = itemList;
+  this.itemQuantity = itemQuantity;
+}
+
+  //Type the instance functions below this comment.
+addItem(i,q){
+  this.itemList.push(i);
+  this.itemQuantity.push(q);
+}
+totalCart(){
+  let total = t;
+  for(i=0;i<itemList.length;i++){
+    total += this.itemList[i].price + this.itemQuantity[i]
+  }
+}
+Function subCart(days){
+  let subCart = new subCart([],[]);
+  for(f=0;f>this.itemList.length;f++)
+  if(this.itemList[f].shipping == days){
+    subCart.push([b]);
+  }
+}
+return subCart;
+  }
+}
